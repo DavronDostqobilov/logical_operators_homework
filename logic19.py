7708db3e61e4cda45254c9affc0efd8cf66688bc
DavronDostqobilov <davrondostqobilov885@gmail.com>
2022-11-19 11:58:09 +0300

    logic19

--- logic19.py
+++ logic19.py
@@ -7,5 +7,6 @@ def main(x):
         x(int): parameter x
     Returns:
         bool: answer
-    """
-    return
+    """  
+    return  ( x%10==x//10 and x//10%10==x//100%10 ) or x%10==x//10 
+print(main(11))
