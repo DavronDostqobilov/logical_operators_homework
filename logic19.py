6bc585063ac5fc5ffee260c8af3ccd6265435d6c
DavronDostqobilov <davrondostqobilov885@gmail.com>
2022-11-19 12:01:03 +0300

    logic19

--- logic19.py
+++ logic19.py
@@ -8,5 +8,5 @@ def main(x):
     Returns:
         bool: answer
     """  
-    return  ( x%10==x//10 and x//10%10==x//100%10 ) or x%10==x//10 
+    return  ( x%10==x//1000 and x//10%10==x//100%10 ) or x%10==x//10 
 print(main(11))
